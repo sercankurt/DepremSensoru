ab5bb15de1ac32e4e0b51ab067ad616ea04d22e0
sercankurt <109027975+sercankurt@users.noreply.github.com>
2023-02-21 14:45:56 +0300

    Update DepremSensorXIAO.ino

--- DepremSensorXIAO.ino
+++ DepremSensorXIAO.ino
@@ -1,77 +1,91 @@
+#include <ESP8266WiFi.h>  //Esp8266-01 Kütüphanesi
+#include <TinyMPU6050.h>  //MPU gyrp sensörü küütüpanesi
 
-#include <Wire.h>
-#include <TinyMPU6050.h>
+const char* ssid = "TA1AFS";   //esp modem bağlantısı
+const char* password = "?sngMuDvk@#";  //esp modem bağlantısı
 
-float pos_offset = 1;
-float neg_offset = -1;
+float pos_offset = 0.90;  // MPU hassasiyet
+float neg_offset = -0.90; // MPU hassasiyet
 long angle_x, angle_y, angle_z, offset_x, offset_y, offset_z;
-MPU6050 mpu (Wire);
+MPU6050 mpu (Wire);  // MPU'yu I2C olarak cihaza tanıtmak için
 
 
 void setup() {
+
+  Wire.begin(0, 2);  // I2C pinleri tanıtıldı. 0=SDA, 2=SCL
 
   // Initialization
   mpu.Initialize();
 
   // Calibration
-  SerialUSB.begin(9600);
-  SerialUSB.println("=====================================");
-  SerialUSB.println("Starting calibration...");
+  Serial.begin(9600);
+  Serial.println("=====================================");
+  Serial.println("Starting calibration...");
   mpu.Calibrate();
-  SerialUSB.println("Calibration complete!");
-  SerialUSB.println("Offsets:");
-  SerialUSB.print("AccX Offset = "); SerialUSB.println(mpu.GetAccX());
-  SerialUSB.print("AccY Offset = "); SerialUSB.println(mpu.GetAccY());
-  SerialUSB.print("AccZ Offset = "); SerialUSB.println(mpu.GetAccZ());
-  SerialUSB.print("GyroX Offset = ");SerialUSB.println(mpu.GetGyroXOffset());
-  SerialUSB.print("GyroY Offset = ");SerialUSB.println(mpu.GetGyroYOffset());
-  SerialUSB.print("GyroZ Offset = ");SerialUSB.println(mpu.GetGyroZOffset());
+  Serial.println("Calibration complete!");
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  delay(1000);
-  for(int i=0; i<50;i++){
+    for(int i=0; i<200; i++){
    mpu.Execute();
   offset_x = mpu.GetAngX();
   offset_y = mpu.GetAngY();
   offset_z = mpu.GetAngZ();
   }
-  SerialUSB.print("offset_x = ");
-  SerialUSB.print(offset_x);
-  SerialUSB.print("  /  offsetY = ");
-  SerialUSB.print(offset_y);
-  SerialUSB.print("  /  offsetZ = ");
-  SerialUSB.println(offset_z);
   
+  Serial.println("Offsets:");
+  Serial.print("GyroX Offset = ");
+  Serial.println(mpu.GetGyroXOffset());
+  Serial.print("GyroY Offset = ");
+  Serial.println(mpu.GetGyroYOffset());
+  Serial.print("GyroZ Offset = ");
+  Serial.println(mpu.GetGyroZOffset());
+
+  pinMode(3, OUTPUT);  //Tx 01 pini çıkış pini olarak tanıtıldı
+  digitalWrite(3, LOW); //1.pin lojik 0'a çekildi
+
+  
+  digitalWrite(3, HIGH); //1.pin lojik 0'a çekildiz
+  delay(1000);
+  digitalWrite(3, LOW); //1.pin lojik 0'a çekildiz
+  delay(1000);
+  digitalWrite(3, HIGH); //1.pin lojik 0'a çekildiz
+  delay(1000);
+  digitalWrite(3, LOW); //1.pin lojik 0'a çekildiz
+  delay(1000);
+
 }
 
 void loop() {
 
+ for(int i=0; i<10; i++){
   mpu.Execute();
   angle_x = mpu.GetAngX();
   angle_y = mpu.GetAngY();
-  angle_z = mpu.GetAngZ();
- 
-  SerialUSB.print("AngX = ");
-  SerialUSB.print(angle_x - offset_x);
-  SerialUSB.print("  /  AngY = ");
-  SerialUSB.print(angle_y - offset_y);
-  SerialUSB.print("  /  AngZ = ");
-  SerialUSB.println(angle_z - offset_z);
+  angle_z = mpu.GetAngZ(); }
+
+  Serial.print("AngX = ");
+  Serial.print(mpu.GetAngX());
+  Serial.print("  /  AngY = ");
+  Serial.print(mpu.GetAngY());
+  Serial.print("  /  AngZ = ");
+  Serial.println(mpu.GetAngZ());
+
+  if ( pos_offset < angle_x - offset_x ||
+       neg_offset > angle_x - offset_x ||
+       pos_offset < angle_y - offset_y ||
+       neg_offset > angle_y - offset_y ||
+       pos_offset < angle_z - offset_z ||
+       neg_offset > angle_z - offset_z) {
+
   
+      digitalWrite(3, HIGH);
+      delay(50);
+      digitalWrite(3, LOW);
+      delay(50);
+    
 
- if ( pos_offset < angle_x - offset_x || neg_offset > angle_x - offset_x || pos_offset < angle_y - offset_y || neg_offset > angle_y - offset_y || pos_offset < angle_z - offset_z || neg_offset > angle_z - offset_z){
-
-
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(50);
-  digitalWrite(LED_BUILTIN,HIGH);
-  delay(50);
-      
-  mpu.Execute();
-  offset_x = mpu.GetAngX();
-  offset_y = mpu.GetAngY();
-  offset_z = mpu.GetAngZ();
- }  
+    mpu.Execute();
+    offset_x = mpu.GetAngX();
+    offset_y = mpu.GetAngY();
+    offset_z = mpu.GetAngZ();
+  }
 }
